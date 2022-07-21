2e0f80df57ab6c3e14196961c83e78f63ba6baea
mutahir <mutahir1993@gmail.com>
2022-07-21 15:22:58 +0530

    jekns

---
+++ main.tf
@@ -0,0 +1,50 @@
+# PROVIDERS
+# //////////////////////////////
+provider "aws" {
+  region     = var.region
+}
+
+# //////////////////////////////
+# RESOURCES
+# //////////////////////////////
+
+# VPC
+resource "aws_vpc" "vpc1" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = "true"
+}
+
+# SUBNET
+resource "aws_subnet" "subnet1" {
+  cidr_block = var.subnet1_cidr
+  vpc_id = aws_vpc.vpc1.id
+  map_public_ip_on_launch = "true"
+  availability_zone = data.aws_availability_zones.available.names[1]
+}
+# SUBNET
+resource "aws_subnet" "subnet2" {
+  cidr_block = var.subnet2_cidr
+  vpc_id = aws_vpc.vpc1.id
+  map_public_ip_on_launch = "true"
+  availability_zone = data.aws_availability_zones.available.names[2]
+}
+
+# INTERNET_GATEWAY
+resource "aws_internet_gateway" "gateway1" {
+  vpc_id = aws_vpc.vpc1.id
+}
+
+# ROUTE_TABLE
+resource "aws_route_table" "route_table1" {
+  vpc_id = aws_vpc.vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gateway1.id
+  }
+}
+
+resource "aws_route_table_association" "route-subnet1" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.route_table1.id
+}
